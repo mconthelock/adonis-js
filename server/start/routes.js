a90d2a12edc95b76ab1d7fd18f5cf3a9bfd9b76f
McOnthelock <s.chalormsak@gmail.com>
2021-01-02 22:36:19 +0700

    Project Module

--- server/start/routes.js
+++ server/start/routes.js
@@ -19,5 +19,10 @@ const Route = use('Route')
 Route.group(() => {
     Route.post('auth/register', 'UserController.register');
     Route.post('auth/login', 'UserController.login');
+
+    Route.get('projects', 'ProjectController.index').middleware('auth');
+    Route.post('projects', 'ProjectController.create').middleware('auth');
+    Route.delete('projects/:id', 'ProjectController.destroy').middleware('auth');
+    Route.patch('projects/:id', 'ProjectController.update').middleware('auth');
 })
 .prefix('api');
